3069d61cf7bd247270c70bdb2305f37bf0ff7d24
huangyu1996 <sdu_huangyu@foxmail.com>
2018-12-05 13:33:24 +0800

    HBASE　ＣＵＲＤ

--- src/main/java/com/learn/hbase/TestHbase.java
+++ src/main/java/com/learn/hbase/TestHbase.java
@@ -12,25 +12,49 @@ import java.io.IOException;
 
 public class TestHbase {
     //判断表是否存在
-
-
-    public static boolean old_tableExist(String tableName) throws IOException {
-        HBaseConfiguration configuration = new HBaseConfiguration();
-        configuration.set("hbase.zookeeper.quorum", "192.168.37.100");
-        //获取hbase的管理员对象
-        HBaseAdmin hBaseAdmin = new HBaseAdmin(configuration);
-        boolean tableExists = hBaseAdmin.tableExists(tableName);
-        hBaseAdmin.close();
-        return tableExists;
-    }
-
-    private static boolean tableExist(String tableName) throws IOException {
-//        HBaseConfiguration configuration = new HBaseConfiguration();
+    private static Admin admin = null;
+    static{
         Configuration configuration = HBaseConfiguration.create();
         configuration.set("hbase.zookeeper.quorum", "192.168.37.100");
         /* 获取hbase的管理员对象 */
-        Connection connection = ConnectionFactory.createConnection(configuration);
-        Admin admin = connection.getAdmin();
+        Connection connection = null;
+        try {
+            connection = ConnectionFactory.createConnection(configuration);
+            admin = connection.getAdmin();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void close(Connection conn,Admin admin){
+        if(conn!=null){
+            try {
+                conn.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if(admin!=null){
+            try {
+                admin.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+//    public static boolean old_tableExist(String tableName) throws IOException {
+//        HBaseConfiguration configuration = new HBaseConfiguration();
+//        configuration.set("hbase.zookeeper.quorum", "192.168.37.100");
+//        //获取hbase的管理员对象
+//        HBaseAdmin hBaseAdmin = new HBaseAdmin(configuration);
+//        boolean tableExists = hBaseAdmin.tableExists(tableName);
+//        hBaseAdmin.close();
+//        return tableExists;
+//    }
+
+    private static boolean tableExist(String tableName) throws IOException {
+//        HBaseConfiguration configuration = new HBaseConfiguration();
+
         boolean tableExists = admin.tableExists(TableName.valueOf(tableName));
         admin.close();
         return tableExists;
@@ -38,7 +62,10 @@ public class TestHbase {
 
 
     //创建表
+    public void createTable(String tableName,String... cfs){
 
+
+    }
 
     //删除表
     //CRUD
