458d22f6d2f1a1f8681cbfe73d952534e899b2b8
huangyu1996 <sdu_huangyu@foxmail.com>
2018-12-05 13:06:32 +0800

    Hbase

--- src/main/java/com/learn/hbase/TestHbase.java
+++ src/main/java/com/learn/hbase/TestHbase.java
@@ -1,14 +1,33 @@
 package com.learn.hbase;
 
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.junit.Test;
+
+import java.io.IOException;
+
 public class TestHbase {
     //判断表是否存在
-    public boolean tableExist(String tableName){
-        new
+
+    @Test
+    public static  boolean tableExist(String tableName) throws IOException {
+        HBaseConfiguration configuration = new HBaseConfiguration();
+        configuration.set("hbase.zookeeper.quorum", "192.168.37.100");
+        //获取hbase的管理员对象
+        HBaseAdmin hBaseAdmin = new HBaseAdmin(configuration);
+        boolean tableExists = hBaseAdmin.tableExists(tableName);
+        hBaseAdmin.close();
+        return tableExists;
     }
+
+
+
     //创建表
     //删除表
     //CRUD
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        System.out.println("123");
+        System.out.println(tableExist("staff"));
+        System.out.println(tableExist("student"));
     }
 }
