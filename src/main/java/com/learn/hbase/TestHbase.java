60fba0947ab3459731acefd2cd22ea4bddde9cb0
huangyu1996 <sdu_huangyu@foxmail.com>
2018-12-05 13:13:08 +0800

    Hbase高低级API

--- src/main/java/com/learn/hbase/TestHbase.java
+++ src/main/java/com/learn/hbase/TestHbase.java
@@ -1,16 +1,20 @@
 package com.learn.hbase;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Admin;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
 import org.apache.hadoop.hbase.client.HBaseAdmin;
-import org.junit.Test;
 
 import java.io.IOException;
 
 public class TestHbase {
     //判断表是否存在
 
-    @Test
-    public static  boolean tableExist(String tableName) throws IOException {
+
+    public static boolean old_tableExist(String tableName) throws IOException {
         HBaseConfiguration configuration = new HBaseConfiguration();
         configuration.set("hbase.zookeeper.quorum", "192.168.37.100");
         //获取hbase的管理员对象
@@ -20,9 +24,22 @@ public class TestHbase {
         return tableExists;
     }
 
+    private static boolean tableExist(String tableName) throws IOException {
+//        HBaseConfiguration configuration = new HBaseConfiguration();
+        Configuration configuration = HBaseConfiguration.create();
+        configuration.set("hbase.zookeeper.quorum", "192.168.37.100");
+        /* 获取hbase的管理员对象 */
+        Connection connection = ConnectionFactory.createConnection(configuration);
+        Admin admin = connection.getAdmin();
+        boolean tableExists = admin.tableExists(TableName.valueOf(tableName));
+        admin.close();
+        return tableExists;
+    }
 
 
     //创建表
+
+
     //删除表
     //CRUD
     public static void main(String[] args) throws IOException {
